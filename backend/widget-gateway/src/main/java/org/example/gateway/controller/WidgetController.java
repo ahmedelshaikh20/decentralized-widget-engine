ecc5096c055c350fab0a50f5270f20b0e3508929
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-11-29 03:37:17 +0100

    feat(gateway): creating controller

---
+++ backend/widget-gateway/src/main/java/org/example/gateway/controller/WidgetController.java
@@ -0,0 +1,31 @@
+package org.example.gateway.controller;
+
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.example.gateway.model.Platform;
+import org.example.gateway.model.WidgetResponse;
+import org.example.gateway.service.WidgetService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WidgetController {
+
+  public WidgetService widgetService;
+  public WidgetController(WidgetService service) {
+    this.widgetService = service;
+  }
+
+  @GetMapping("api/v1/widgets")
+  public WidgetResponse getWidgets(
+    @RequestParam String userId,
+    @RequestParam(defaultValue = "WEB") Platform platform
+  ) {
+    return widgetService.getWidgetsForUser(userId, platform);
+  }
+
+
+
+}
