b749c55a85180b8f47ab9df9a6966ac498adc048
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-06 03:46:52 +0100

    feat(ui) : adding top bar and bottom bar for main activity

--- app/src/main/java/com/example/check24_android/MainActivity.kt
+++ app/src/main/java/com/example/check24_android/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.check24_android.ui.theme.Check24androidTheme
+import androidx.compose.ui.unit.dp
+import com.example.check24_android.presentation.screens.dashboard.DashboardScreen
+import com.example.check24_android.presentation.ui.theme.BrandBlueDark
+import com.example.check24_android.presentation.ui.theme.BrandWhite
+import com.example.check24_android.presentation.ui.theme.Check24androidTheme
+import com.example.check24_android.ui.components.Check24BottomBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +27,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Check24androidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize(),
+                  topBar ={
+                    AppTopBar()
+                  },
+                  bottomBar = {
+                    Check24BottomBar(
+                      currentRoute = "home",
+                      onNavigate = { newRoute ->
+                      }
                     )
+                  }
+
+                ) { innerPadding ->
+                  DashboardScreen(modifier = Modifier.padding(innerPadding).padding(16.dp))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    Check24androidTheme {
-        Greeting("Android")
-    }
+fun AppTopBar() {
+  CenterAlignedTopAppBar(
+    title = {
+      Text(
+        text = "CHECK24 Home",
+        style = MaterialTheme.typography.titleLarge,
+        color = BrandWhite
+      )
+    },
+    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+      containerColor = BrandBlueDark,
+      titleContentColor = BrandWhite,
+      actionIconContentColor = BrandWhite,
+      navigationIconContentColor = BrandWhite
+    )
+  )
 }
