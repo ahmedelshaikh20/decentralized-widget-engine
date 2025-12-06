71216fd87e1787b3fcdf82f1949580f5e8afca8d
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-06 03:30:37 +0100

    feat(common): creating temp home nav bar

---
+++ app/src/main/java/com/example/check24_android/presentation/ui/common/BottomNavBar.kt
@@ -0,0 +1,97 @@
+package com.example.check24_android.ui.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.check24_android.presentation.ui.theme.AppCardDefaults
+import com.example.check24_android.presentation.ui.theme.BottomNavDefaults
+import com.example.check24_android.presentation.ui.theme.BrandWhite
+import com.example.check24_android.presentation.ui.theme.Check24androidTheme
+import com.example.check24_android.presentation.ui.theme.NavSelected
+import com.example.check24_android.presentation.ui.theme.NavUnselected
+
+sealed class BottomNavItem(
+  val title: String,
+  val icon: ImageVector,
+  val route: String
+) {
+  object Home : BottomNavItem("Home", Icons.Filled.Home, "home")
+  object Flights : BottomNavItem("Flights", Icons.Filled.ArrowForward, "flights")
+  object Insurance : BottomNavItem("Insurance", Icons.Filled.Star, "insurance")
+  object Internet : BottomNavItem("Internet", Icons.Filled.Search, "internet")
+  object Loans : BottomNavItem("Loans", Icons.Filled.MailOutline, "loans")
+}
+
+
+  // Observe current route to set selected state
+//  val navBackStackEntry by navController.currentBackStackEntryAsState()
+//  val currentRoute = navBackStackEntry?.destination?.route
+
+  @Composable
+  fun Check24BottomBar(
+    currentRoute: String,
+    onNavigate: (String) -> Unit
+  ) {
+    val items = listOf(
+      BottomNavItem.Home,
+      BottomNavItem.Flights,
+      BottomNavItem.Insurance,
+      BottomNavItem.Internet,
+      BottomNavItem.Loans
+    )
+
+    NavigationBar(
+      containerColor = BrandWhite,
+      tonalElevation = AppCardDefaults.elevation,
+    ) {
+      items.forEach { item ->
+        val isSelected = currentRoute == item.route
+
+        NavigationBarItem(
+          label = {
+            Text(
+              text = item.title,
+              style = MaterialTheme.typography.labelLarge,
+              color = if (isSelected) NavSelected else NavUnselected
+            )
+          },
+          icon = {
+            Icon(
+              imageVector = item.icon,
+              contentDescription = item.title,
+              modifier = Modifier.size(BottomNavDefaults.iconSize), // 24.dp
+              tint = if (isSelected) NavSelected else NavUnselected
+            )
+          },
+          selected = isSelected,
+          onClick = { onNavigate(item.route) },
+
+          colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = NavSelected,
+            selectedTextColor = NavSelected,
+            indicatorColor = Color.Transparent,
+            unselectedIconColor = NavUnselected,
+            unselectedTextColor = NavUnselected
+          )
+        )
+      }
+    }
+  }
+
+
+@Preview(showBackground = true)
+@Composable
+fun BottomBarPreview() {
+  Check24androidTheme {
+    Check24BottomBar(
+      currentRoute = "home",
+      onNavigate = {}
+    )
+  }
+}
