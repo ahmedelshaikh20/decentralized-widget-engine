64e88d8f15fff3f198f2876eeb23954f1f9f3c36
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-10 14:48:28 +0100

    fix(shared) : adding some attributes to the widget data model

---
+++ backend/shared/src/main/java/org/example/shared/models/CarouselItem.java
@@ -0,0 +1,8 @@
+package org.example.shared.models;
+
+public record CarouselItem(
+        String title,
+        String imageUrl,
+        String ctaLabel,
+        String ctaUrl
+) {}
--- backend/shared/src/main/java/org/example/shared/models/WidgetData.java
+++ backend/shared/src/main/java/org/example/shared/models/WidgetData.java
@@ -1,8 +1,14 @@
 package org.example.shared.models;
 
+
+import java.util.List;
+
 public record WidgetData(
-    String title,
-    String subtitle,
-    String ctaLabel,
-    String ctaUrl
+  String title,
+  String subtitle,
+  String imageUrl,
+  String iconUrl,
+  List<CarouselItem> items,
+  String ctaLabel,
+  String ctaUrl
 ) {}
