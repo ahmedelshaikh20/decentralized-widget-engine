512021636b51a78806863f1357eb01bd324693b7
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-14 17:59:39 +0100

    fix(gateway): fixing redis config

--- backend/widget-gateway/src/main/java/org/example/gateway/config/RedisConfig.java
+++ backend/widget-gateway/src/main/java/org/example/gateway/config/RedisConfig.java
@@ -1,7 +1,6 @@
 package org.example.gateway.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,19 +15,19 @@ public class RedisConfig {
 
 
   @Bean
-  public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-    RedisTemplate<String, Object> template = new RedisTemplate<>();
+  public RedisTemplate<String, String> redisTemplate(
+    RedisConnectionFactory factory
+  ) {
+    RedisTemplate<String, String> template = new RedisTemplate<>();
     template.setConnectionFactory(factory);
-
-    // Same serializers as Processor
     template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new StringRedisSerializer());
     template.setHashKeySerializer(new StringRedisSerializer());
-    template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-    template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
+    template.setHashValueSerializer(new StringRedisSerializer());
     template.afterPropertiesSet();
     return template;
   }
+
 
   @Bean
   public ObjectMapper objectMapper() {
