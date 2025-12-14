a832768c06f355a4855c5536d49e296e208981f6
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-14 19:08:13 +0100

    feat(gateway): creating fallback widget service

--- backend/widget-gateway/src/main/java/org/example/gateway/service/WidgetService.java
+++ backend/widget-gateway/src/main/java/org/example/gateway/service/WidgetService.java
@@ -3,10 +3,10 @@ package org.example.gateway.service;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import org.example.gateway.repository.WidgetCacheRepository;
-import org.example.shared.models.Platform;
-import org.example.shared.models.WidgetCacheDto;
-import org.example.shared.models.WidgetDto;
-import org.example.shared.models.WidgetResponse;
+import org.example.shared.models.widgets.Platform;
+import org.example.shared.models.widgets.WidgetCacheDto;
+import org.example.shared.models.widgets.WidgetDto;
+import org.example.shared.models.widgets.WidgetResponse;
 import org.springframework.stereotype.Service;
 import java.util.Comparator;
 import java.util.List;
@@ -18,9 +18,11 @@ public class WidgetService {
 
   private final WidgetCacheRepository repository;
   private final CircuitBreaker circuitBreaker;
-  public WidgetService(WidgetCacheRepository repository, CircuitBreakerRegistry circuitBreakerRegistry) {
+  private final FallbackWidgetService fallbackService;
+  public WidgetService(WidgetCacheRepository repository, CircuitBreakerRegistry circuitBreakerRegistry , FallbackWidgetService fallbackService) {
     this.repository = repository;
     this.circuitBreaker = circuitBreakerRegistry.circuitBreaker("widgetCache");
+    this.fallbackService = fallbackService;
   }
 
 
@@ -36,13 +38,13 @@ public class WidgetService {
     List<WidgetCacheDto> source;
     try {
       source = protectedCall.get();
+      if (source.isEmpty()) {
+        source =fallbackService.getCriticalWidgets(platform);
+      }
     } catch (Exception ex) {
-      source = List.of();
+      source = fallbackService.getCriticalWidgets(platform);
     }
 
-    if (source.isEmpty()) {
-      return new WidgetResponse(List.of());
-    }
     List<WidgetDto> result = source.stream()
       .filter(widget -> widget.platformVisibility().contains(platform))
       .sorted(Comparator.comparingInt(WidgetCacheDto::priority).reversed())
@@ -53,3 +55,7 @@ public class WidgetService {
   }
 
 }
+
+
+
+
