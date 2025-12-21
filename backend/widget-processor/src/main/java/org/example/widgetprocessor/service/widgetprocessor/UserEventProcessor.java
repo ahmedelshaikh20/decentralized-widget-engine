5fae7b7cca0b35a7ef67bcf974ac77e8370a9d7b
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-21 23:47:19 +0100

    modifying yaml files and metadata names

--- backend/widget-processor/src/main/java/org/example/widgetprocessor/service/widgetprocessor/UserEventProcessor.java
+++ backend/widget-processor/src/main/java/org/example/widgetprocessor/service/widgetprocessor/UserEventProcessor.java
@@ -3,41 +3,38 @@ package org.example.widgetprocessor.service.widgetprocessor;
 import org.example.widgetprocessor.model.userevent.UserEvent;
 import org.example.widgetprocessor.repository.UserHistoryRepository;
 import org.example.widgetprocessor.repository.WidgetCacheWriteRepository;
-import org.example.widgetprocessor.service.aiservice.DSAiWidgetService;
 import org.example.widgetprocessor.service.ruleservice.RuleEngineService;
 import org.example.widgetprocessor.service.widgetservice.WidgetBuilderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 @Service
 public class UserEventProcessor {
 
+  private static final Logger log = LoggerFactory.getLogger(UserEventProcessor.class);
   private final RuleEngineService ruleEngine;
   private final WidgetBuilderService builder;
   private final WidgetCacheWriteRepository widgetRepository;
   private final UserHistoryRepository userHistoryRepository;
-  private final DSAiWidgetService aiWidgetService;
 
-  public UserEventProcessor(RuleEngineService ruleEngine, WidgetBuilderService builder, WidgetCacheWriteRepository cache , UserHistoryRepository userHistoryRepository , DSAiWidgetService aiWidgetService) {
+  public UserEventProcessor(RuleEngineService ruleEngine, WidgetBuilderService builder, WidgetCacheWriteRepository cache , UserHistoryRepository userHistoryRepository) {
     this.ruleEngine = ruleEngine;
     this.builder = builder;
     this.widgetRepository = cache;
     this.userHistoryRepository = userHistoryRepository;
-    this.aiWidgetService = aiWidgetService;
   }
 
   public void processEvent(UserEvent event) {
+    log.debug("New Event: {}", event.toString());
+
     String userId = event.getUserId();
-    storeEventHistory(event);
+ //   storeEventHistory(event);
     var history = userHistoryRepository.getLastEvents(userId);
     var rules = ruleEngine.calculateEligibleWidgets(event);
 
     if (rules.isEmpty()) return;
     var lastMeta = userHistoryRepository.getLastMetadata(userId, event.getEventType());
-
 
     for (var rule : rules) {
       var widget = builder.build(event, rule ,lastMeta , history);
@@ -50,22 +47,22 @@ public class UserEventProcessor {
   }
 
 
-  private void storeEventHistory(UserEvent event) {
-    Map<String, Object> entry = new HashMap<>();
-    entry.put("eventType", event.getEventType());
-    entry.put("productId", event.getProductId());
-    entry.put("timestamp", event.getTimestamp());
-    entry.put("metadata", event.getMetadata());
-
-    userHistoryRepository.pushEvent(event.getUserId(), entry);
-
-    // Save metadata snapshot for this event type
-    userHistoryRepository.updateLastMetadata(
-      event.getUserId(),
-      event.getEventType().toUpperCase(Locale.ROOT),
-      event.getMetadata()
-    );
-  }
+//  private void storeEventHistory(UserEvent event) {
+//    Map<String, Object> entry = new HashMap<>();
+//    entry.put("eventType", event.getEventType());
+//    entry.put("productId", event.getProductId());
+//    entry.put("timestamp", event.getTimestamp());
+//    entry.put("metadata", event.getMetadata());
+//
+//    userHistoryRepository.pushEvent(event.getUserId(), entry);
+//
+//    // Save metadata snapshot for this event type
+//    userHistoryRepository.updateLastMetadata(
+//      event.getUserId(),
+//      event.getEventType().toUpperCase(Locale.ROOT),
+//      event.getMetadata()
+//    );
+//  }
 
 
 }
