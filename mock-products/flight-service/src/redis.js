7bb2acfb194c6e4931cf0138d745dabe23144f96
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-22 01:08:51 +0100

    updating the mock products server with necessary mock functions

--- mock-products/flight-service/src/redis.js
+++ mock-products/flight-service/src/redis.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const redis = new Redis({
-  host: process.env.REDIS_HOST || "localhost",
+  host: process.env.REDIS_HOST || "redis",
   port: Number(process.env.REDIS_PORT || 6379),
   password: process.env.REDIS_PASSWORD || undefined
 });
