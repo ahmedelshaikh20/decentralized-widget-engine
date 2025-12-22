3ecaa9f691a34d6d4c66d084b26d298f03983995
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-12-22 02:37:56 +0100

    enabling cors for all for testing purpose

--- backend/widget-gateway/src/main/java/org/example/gateway/config/CorsConfig.java
+++ backend/widget-gateway/src/main/java/org/example/gateway/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/api/v1/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
